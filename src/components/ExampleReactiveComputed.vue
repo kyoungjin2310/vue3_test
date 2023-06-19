353fcae70e53c7c52376de808f5037bdb7e8ebcc
kyeongjin <alicia2310@seize.kr>
2023-06-19 17:09:41 +0900

    feat:[computed] vue 3 setup computed 사용 방법

---
+++ src/components/ExampleReactiveComputed.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>Has published books:</p>
+    <span>{{ dataLength }}</span>
+  </div>
+</template>
+<script setup>
+import { reactive, computed } from "vue";
+
+const data = reactive({
+  name: "data",
+  books: ["data1", "data2", "data3"],
+});
+
+//api 받은 후 computed 사용 방법
+const dataLength = computed(() => {
+  return data.books.length > 0 ? "Y" : "N";
+});
+</script>
+
+<style></style>
