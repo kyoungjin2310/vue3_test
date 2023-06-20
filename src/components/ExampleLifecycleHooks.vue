d31f04dea5d9036ba01e16a264668519d6576ebe
kyeongjin <alicia2310@seize.kr>
2023-06-20 18:00:07 +0900

    feat:[onMounted] setup mounted 사용 방법

---
+++ src/components/ExampleLifecycleHooks.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- onMounted - 컴포넌트가 마운트된 후 호출, 콜백 -->
+  <div ref="el"></div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+
+const el = ref();
+
+onMounted(() => {
+  console.log(el.value);
+});
+</script>
+
+<style lang="scss" scoped></style>
