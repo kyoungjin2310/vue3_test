bb999452ce655039f1910c2bac9cbf91a90e546d
kyeongjin <alicia2310@seize.kr>
2023-06-20 15:40:32 +0900

    feat:[reactive] v-for reactive 사용

---
+++ src/components/ExampleVForReactive.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(val, key, idx) in obj" :key="idx">
+        val:{{ val }} key:{{ key }} idx:{{ idx }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { reactive } from "vue";
+
+const obj = reactive({
+  title: "title",
+  name: "name",
+  text: "text",
+});
+</script>
+
+<style lang="scss" scoped>
+.color {
+  color: skyblue;
+}
+.active {
+  color: brown;
+}
+</style>
