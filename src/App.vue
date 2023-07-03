7b32d75287ffce76370df7b719edcb989c425d3f
kyeongjin <alicia2310@seize.kr>
2023-07-03 17:55:50 +0900

    feat: [props] setup에서 props 사용 방법

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
+    <title-style title="Vue3 test" />
     <example-parents />
   </div>
 </template>
 
 <script>
 import ExampleParents from "@/components/DOM/setData/ExampleParents.vue";
+import TitleStyle from "@/components/style/TitleStyle.vue";
 import "./assets/scss/style.scss";
 export default {
   name: "App",
   components: {
     ExampleParents,
+    TitleStyle,
   },
 };
 </script>
---
+++ src/components/style/TitleStyle.vue
@@ -0,0 +1,14 @@
+<template>
+  <h2 class="title">{{ title }}</h2>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+defineProps(["title"]);
+</script>
+
+<style lang="scss" scoped>
+.title {
+  font-size: 32px;
+}
+</style>
