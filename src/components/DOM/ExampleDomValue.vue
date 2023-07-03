b44e3936ed5692f9af6602a0a636a970806f43a0
kyeongjin <alicia2310@seize.kr>
2023-07-03 17:23:03 +0900

    feat:[DOM] ref 이름대신 함수 전달 방법

--- src/components/DOM/ExampleDomValue.vue
+++ src/components/DOM/ExampleDomValue.vue
@@ -1,15 +1,23 @@
 <template>
-  <!-- DOM focus -->
-  <input ref="input" />
+  <!-- DOM ref참조 이름 문자열 대신 함수를 전달할 수 있도록 동적 바인딩을 사용-->
+  <input
+    :ref="
+      (el) => {
+        el.focus();
+        console.log(el.value);
+      }
+    "
+    value="test"
+  />
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
+import { onMounted } from "vue";
 
-const input = ref(null);
+// const input = ref(null);
 
 onMounted(() => {
-  input.value.focus();
+  //   input.value.focus();
 });
 </script>
 
