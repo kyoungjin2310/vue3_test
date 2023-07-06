47e3d58605f7fe7a290768ffd2ee7a306435f66c
kyeongjin <alicia2310@seize.kr>
2023-07-06 17:51:07 +0900

    feat: [props] vue3 props 쓰는 방법

--- src/components/DOM/setData/ExampleChild.vue
+++ src/components/DOM/setData/ExampleChild.vue
@@ -1,13 +1,21 @@
 <template>
   <!-- 자식컴포넌트 -->
-  <div>children</div>
+  <div>{{ propTitle }}</div>
 </template>
 
 <script setup>
-import { ref, defineExpose } from "vue";
+import { ref, defineExpose, defineProps } from "vue";
 
 const a = 1;
 const b = ref(2);
+const props = defineProps({
+  title: {
+    Type: String,
+    default: "",
+  },
+});
+
+const propTitle = ref(props.title);
 
 //부모컴포넌트에 데이터 전달
 defineExpose({
--- src/components/DOM/setData/ExampleParents.vue
+++ src/components/DOM/setData/ExampleParents.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- 부모컴포넌트 : 자식컴포넌트 value값 참조 -->
   <div>
-    <example-child ref="child" />
+    <example-child ref="child" :title="text" />
     <p>{{ childval }}</p>
   </div>
 </template>
@@ -13,6 +13,7 @@ import { ref, onMounted, computed } from "vue";
 
 const child = ref(null);
 const childval = computed(() => child?.value?.b + child?.value?.a);
+const text = ref("test t");
 
 onMounted(() => {
   // eslint-disable-next-line
