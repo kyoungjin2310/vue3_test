55bdaefb4ec2567b141497466a01a315e974f9b7
kyeongjin <alicia2310@seize.kr>
2023-07-06 17:35:26 +0900

    feat:[computed] 자식 데이터 computed 적용

--- src/components/DOM/setData/ExampleParents.vue
+++ src/components/DOM/setData/ExampleParents.vue
@@ -2,18 +2,22 @@
   <!-- 부모컴포넌트 : 자식컴포넌트 value값 참조 -->
   <div>
     <example-child ref="child" />
+    <p>{{ childval }}</p>
   </div>
 </template>
 
 <script setup>
 import ExampleChild from "./ExampleChild.vue";
 // eslint-disable-next-line
-import { ref, onMounted } from "vue";
+import { ref, onMounted, computed } from "vue";
 
 const child = ref(null);
+const childval = computed(() => child?.value?.b + child?.value?.a);
+
 onMounted(() => {
   // eslint-disable-next-line
   console.log(child.value.b);
+  console.log(child.value.a);
 });
 </script>
 
